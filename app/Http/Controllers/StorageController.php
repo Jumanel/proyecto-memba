a2a44b1a2777eae2b3cac5725c65979d2d524a21
Juan Manuel Diz <juanmanueldiz1@gmail.com>
2020-11-30 18:18:09 -0300

    validacion SQLite

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -17,10 +17,29 @@ class StorageController extends Controller
 
        //indicamos que queremos guardar un nuevo archivo en el disco local
        \Storage::disk('local')->put($nombre,  \File::get($file));
+       try {
+		    $nombre = "F:\\laragon\\www\\memba\\storage\\storage\\".$nombre;
+
+		    $conexionBD = new \SQLite3($nombre,SQLITE3_OPEN_READWRITE );
+   		    $conexionBD->enableExceptions(true);
+		    $resultado = $conexionBD->query ("select * from sqlite_master");
+		    $status = "Es SQLite";
+  		}catch (\Exception  $e){
+  			$status = 'No es SQLite';
+  		}
+
 
        //return "archivo guardado";
-       return view('inicio')->with('status','guardado');
+       return view('inicio')->with('status', $status);
+
+
+	}
 
 }
-
-}
+/*  $salida = "Tablas: <br>";
+		    while($row = $resultado->fetchArray(SQLITE3_NUM)){
+			   	for($i=0;$i<count($row);$i++){
+			   		$salida .= $i . ": ". $row[$i]." | ";
+			   	}
+			   	$salida .= "<br>";
+		    } */
